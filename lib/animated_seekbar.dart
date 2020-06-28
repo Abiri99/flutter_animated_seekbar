c0e08ab041f1ace72efa42655d07e07af7a17a25
alireza <abiriali78@gmail.com>
2020-06-28 23:26:23 +0430

    Just needs to be smooth in the beginning of updating seek bar

--- lib/animated_seekbar.dart
+++ lib/animated_seekbar.dart
@@ -23,10 +23,13 @@ class _AnimatedSeekbarState extends State<AnimatedSeekbar>
 
   Animation _seekbarAnimation;
 
+  bool loading;
+
   @override
   void initState() {
+    loading = false;
     _controller = AnimationController(
-      duration: Duration(seconds: 5),
+      duration: Duration(milliseconds: 500),
       vsync: this,
     );
     _seekbarAnimation = CurvedAnimation(
@@ -36,12 +39,18 @@ class _AnimatedSeekbarState extends State<AnimatedSeekbar>
       setState(() {});
     })..addStatusListener((status) {
       if (status == AnimationStatus.dismissed) {
-        _controller.forward();
+//        _controller.forward();
       } else if (status == AnimationStatus.completed) {
-        _controller.reverse();
+        setState(() {
+          loading = false;
+        });
+//        _controller.reverse();
+//        setState(() {
+//          verticalDragOffset = 0;
+//        });
       }
     });
-    _controller.forward();
+//    _controller.forward();
     progress = widget.size.width / 2;
     verticalDragOffset = 0;
     super.initState();
@@ -52,36 +61,37 @@ class _AnimatedSeekbarState extends State<AnimatedSeekbar>
     return Container(
       color: Colors.blue,
       child: GestureDetector(
-//        onHorizontalDragUpdate: (DragUpdateDetails dragUpdateDetails) {
-//          if (dragUpdateDetails.localPosition.dx > 0 &&
-//              dragUpdateDetails.localPosition.dx < widget.size.width)
-//            setState(() {
-////              updating = true;
-//              progress = dragUpdateDetails.localPosition.dx;
-//              verticalDragOffset = dragUpdateDetails.localPosition.dy;
-//            });
-//        },
-//        onHorizontalDragEnd: (DragEndDetails dragEndDetails) {
-//          _controller.forward();
-//        },
+        onHorizontalDragUpdate: (DragUpdateDetails dragUpdateDetails) {
+          if (dragUpdateDetails.localPosition.dx > 0 &&
+              dragUpdateDetails.localPosition.dx < widget.size.width)
+            setState(() {
+//              updating = true;
+              loading = true;
+              progress = dragUpdateDetails.localPosition.dx;
+              verticalDragOffset = dragUpdateDetails.localPosition.dy;
+            });
+        },
+        onHorizontalDragEnd: (DragEndDetails dragEndDetails) {
+          _controller.forward().then((value) => _controller.reset());
+//          _controller.reset();
+        },
         child: AnimatedBuilder(
           animation: _seekbarAnimation,
           builder: (context, child) {
-            print("value: ${_seekbarAnimation.value * 100}");
+            print("value: ${(1 - _seekbarAnimation.value) * verticalDragOffset}");
             return CustomPaint(
               size: widget.size,
               painter: SeekBarPainter(
                 progress: progress,
                 width: widget.size.width,
                 height: widget.size.height,
-                verticalDragOffset: _seekbarAnimation.value * 100
+                verticalDragOffset: loading ? (1 - _seekbarAnimation.value) * verticalDragOffset : 0
               ),
             );
           },
         ),
       ),
     );
-//    );
   }
 
   @override
